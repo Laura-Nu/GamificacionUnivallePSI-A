f90994ac0e1526b21223df7af3d3826d15fd0055
Laura Nuñez <76458689+Laura-Nu@users.noreply.github.com>
2023-11-21 19:59:58 -0400

    Delete DB_Rank_API directory

--- DB_Rank_API/Models/ChangePasswordRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DB_Rank_API.Models
-{
-    public class ChangePasswordRequest
-    {
-        public int UserId { get; set; }
-        public string CurrentPassword { get; set; }
-        public string NewPassword { get; set; }
-    }
-}
